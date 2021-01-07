c190dce1aaac54a96006138f32d862c7abb1c030
Justin Cho <justch0616@gmail.com>
2021-01-07 11:52:51 -0600

    Optimized JS for the Horoscope App

--- cho-cho/horoscopeApp/js/main.js
+++ cho-cho/horoscopeApp/js/main.js
@@ -1,3 +1,4 @@
+let peeps = []
 let dwight = document.getElementById('dwight')
 let jim = document.getElementById('jim')
 let michael = document.getElementById('michael')
@@ -11,191 +12,51 @@ let kelly = document.getElementById('kelly')
 let toby = document.getElementById('toby')
 let pam = document.getElementById('pam')
 
+peeps.push(dwight, jim, michael, angela, kevin, meredith, phyllis, stanley, ryan, kelly, toby, pam)
+
+
 /*FUNCTION*/
 
 const generate = () => {
-    let dob = document.querySelector('#dob').value
-    let dobArray = dob.split('-')
-    /*Capricorn December 22 - January 19*/
-    if (dobArray[1]=='01' && dobArray[2]<= '19' || dobArray[1]=='12' && dobArray[2] >='22') {
-        dwight.classList.remove('hidden')
-        jim.classList.add('hidden')
-        michael.classList.add('hidden')
-        angela.classList.add('hidden')
-        kevin.classList.add('hidden')
-        phyllis.classList.add('hidden')
-        meredith.classList.add('hidden')
-        stanley.classList.add('hidden')
-        ryan.classList.add('hidden')
-        pam.classList.add('hidden')
-        kelly.classList.add('hidden')
-        toby.classList.add('hidden')
-    } 
-    /*Aquarius January 20 - February 18*/
-    if (dobArray[1]=='01' && dobArray[2]>= '20' || dobArray[1]=='02' && dobArray[2] <='18') {
-        pam.classList.remove('hidden')
-        dwight.classList.add('hidden')
-        jim.classList.add('hidden')
-        michael.classList.add('hidden')
-        angela.classList.add('hidden')
-        kevin.classList.add('hidden')
-        phyllis.classList.add('hidden')
-        meredith.classList.add('hidden')
-        stanley.classList.add('hidden')
-        ryan.classList.add('hidden')
-        kelly.classList.add('hidden')
-        toby.classList.add('hidden')
-    } 
-    /* Pisces February 19 - March 20*/
-    if (dobArray[1]=='02' && dobArray[2]>= '19' || dobArray[1]=='03' && dobArray[2] <='20') {
-        pam.classList.add('hidden')
-        dwight.classList.add('hidden')
-        jim.classList.add('hidden')
-        michael.classList.add('hidden')
-        angela.classList.add('hidden')
-        kevin.classList.add('hidden')
-        phyllis.classList.add('hidden')
-        meredith.classList.add('hidden')
-        stanley.classList.add('hidden')
-        ryan.classList.add('hidden')
-        kelly.classList.remove('hidden')
-        toby.classList.add('hidden')
-    } 
-    /*Aries March 21 - April 19 */
-    if (dobArray[1]=='03' && dobArray[2]>= '21' || dobArray[1]=='04' && dobArray[2] <='19') {
-        pam.classList.add('hidden')
-        dwight.classList.add('hidden')
-        jim.classList.add('hidden')
-        michael.classList.add('hidden')
-        angela.classList.add('hidden')
-        kevin.classList.add('hidden')
-        phyllis.classList.add('hidden')
-        meredith.classList.remove('hidden')
-        stanley.classList.add('hidden')
-        ryan.classList.add('hidden')
-        kelly.classList.add('hidden')
-        toby.classList.add('hidden')
-    } 
-    /*Taurus April 20 - May 20*/
-    if (dobArray[1]=='04' && dobArray[2]>= '20' || dobArray[1]=='05' && dobArray[2] <='20') {
-        pam.classList.add('hidden')
-        dwight.classList.add('hidden')
-        jim.classList.add('hidden')
-        michael.classList.add('hidden')
-        angela.classList.add('hidden')
-        kevin.classList.remove('hidden')
-        phyllis.classList.add('hidden')
-        meredith.classList.add('hidden')
-        stanley.classList.add('hidden')
-        ryan.classList.add('hidden')
-        kelly.classList.add('hidden')
-        toby.classList.add('hidden')
-    } 
-    /*Gemini May 21 - June 20*/
-    if (dobArray[1]=='05' && dobArray[2]>= '21' || dobArray[1]=='06' && dobArray[2] <='20') {
-        pam.classList.add('hidden')
-        dwight.classList.add('hidden')
-        jim.classList.add('hidden')
-        michael.classList.add('hidden')
-        angela.classList.add('hidden')
-        kevin.classList.add('hidden')
-        phyllis.classList.add('hidden')
-        meredith.classList.add('hidden')
-        stanley.classList.add('hidden')
-        ryan.classList.remove('hidden')
-        kelly.classList.add('hidden')
-        toby.classList.add('hidden')
-    } 
-    /*Cancer June 21 - July 22*/
-    if (dobArray[1]=='06' && dobArray[2]>= '21' || dobArray[1]=='07' && dobArray[2] <='22') {
-        pam.classList.add('hidden')
-        dwight.classList.add('hidden')
-        jim.classList.add('hidden')
-        michael.classList.add('hidden')
-        angela.classList.add('hidden')
-        kevin.classList.add('hidden')
-        phyllis.classList.add('hidden')
-        meredith.classList.add('hidden')
-        stanley.classList.remove('hidden')
-        ryan.classList.add('hidden')
-        kelly.classList.add('hidden')
-        toby.classList.add('hidden')
-    } 
-    /* leo July 23 - August 22*/
-    if (dobArray[1]=='07' && dobArray[2]>= '23' || dobArray[1]=='08' && dobArray[2] <='22') {
-        pam.classList.add('hidden')
-        dwight.classList.add('hidden')
-        jim.classList.remove('hidden')
-        michael.classList.add('hidden')
-        angela.classList.add('hidden')
-        kevin.classList.add('hidden')
-        phyllis.classList.add('hidden')
-        meredith.classList.add('hidden')
-        stanley.classList.add('hidden')
-        ryan.classList.add('hidden')
-        kelly.classList.add('hidden')
-        toby.classList.add('hidden')
-    } 
-    /*Virgo August 23 - September 22*/
-
-    if (dobArray[1]=='08' && dobArray[2]>= '23' || dobArray[1]=='09' && dobArray[2] <='22') {
-        pam.classList.add('hidden')
-        dwight.classList.add('hidden')
-        jim.classList.add('hidden')
-        michael.classList.add('hidden')
-        angela.classList.add('hidden')
-        kevin.classList.add('hidden')
-        phyllis.classList.remove('hidden')
-        meredith.classList.add('hidden')
-        stanley.classList.add('hidden')
-        ryan.classList.add('hidden')
-        kelly.classList.add('hidden')
-        toby.classList.add('hidden')
-    } 
-    /*Libra September 23 - October 22*/
-    if (dobArray[1]=='09' && dobArray[2]>= '23' || dobArray[1]=='10' && dobArray[2] <='22') {
-        pam.classList.add('hidden')
-        dwight.classList.add('hidden')
-        jim.classList.add('hidden')
-        michael.classList.add('hidden')
-        angela.classList.add('hidden')
-        kevin.classList.add('hidden')
-        phyllis.classList.add('hidden')
-        meredith.classList.add('hidden')
-        stanley.classList.add('hidden')
-        ryan.classList.add('hidden')
-        kelly.classList.add('hidden')
-        toby.classList.remove('hidden')
-    } 
-    /*Scorpio October 23 - November 21*/
-    if (dobArray[1]=='10' && dobArray[2]>= '23' || dobArray[1]=='11' && dobArray[2] <='21') {
-        pam.classList.add('hidden')
-        dwight.classList.add('hidden')
-        jim.classList.add('hidden')
-        michael.classList.add('hidden')
-        angela.classList.remove('hidden')
-        kevin.classList.add('hidden')
-        phyllis.classList.add('hidden')
-        meredith.classList.add('hidden')
-        stanley.classList.add('hidden')
-        ryan.classList.add('hidden')
-        kelly.classList.add('hidden')
-        toby.classList.add('hidden')
+    let dob = document.querySelector('#dob').value;
+    let dobArray = dob.split('-');
+    peeps.forEach(peep => peep.classList.add('hidden'));
+    switch(true) {
+        case dobArray[1]=='01' && dobArray[2]<= '19' || dobArray[1]=='12' && dobArray[2] >='22':
+            dwight.classList.remove('hidden');
+            break;
+        case dobArray[1]=='01' && dobArray[2]>= '20' || dobArray[1]=='02' && dobArray[2] <='18':
+            pam.classList.remove('hidden');
+            break;
+        case dobArray[1]=='02' && dobArray[2]>= '19' || dobArray[1]=='03' && dobArray[2] <='20':
+            kelly.classList.remove('hidden');
+            break;
+        case dobArray[1]=='03' && dobArray[2]>= '21' || dobArray[1]=='04' && dobArray[2] <='19':
+            meredith.classList.remove('hidden');
+            break;
+        case dobArray[1]=='04' && dobArray[2]>= '20' || dobArray[1]=='05' && dobArray[2] <='20':
+            kevin.classList.remove('hidden');
+            break;
+        case dobArray[1]=='05' && dobArray[2]>= '21' || dobArray[1]=='06' && dobArray[2] <='20':
+            ryan.classList.remove('hidden');
+            break;
+        case dobArray[1]=='06' && dobArray[2]>= '21' || dobArray[1]=='07' && dobArray[2] <='22':
+            stanley.classList.remove('hidden');
+            break;
+        case dobArray[1]=='07' && dobArray[2]>= '23' || dobArray[1]=='08' && dobArray[2] <='22':
+            jim.classList.remove('hidden');
+            break;
+        case dobArray[1]=='08' && dobArray[2]>= '23' || dobArray[1]=='09' && dobArray[2] <='22':
+            toby.classList.remove('hidden');
+            break;
+        case dobArray[1]=='10' && dobArray[2]>= '23' || dobArray[1]=='11' && dobArray[2] <='21':
+            angela.classList.remove('hidden');
+            break;
+        case dobArray[1]=='11' && dobArray[2]>= '22' || dobArray[1]=='12' && dobArray[2] <='21':
+            michael.classList.remove('hidden');
+            break;
+        default:
+            console.log('I DECLARE..BANKRUPTCY!!!') 
     }
-     /*Sagittarius November 22 - December 21*/
-    if (dobArray[1]=='11' && dobArray[2]>= '22' || dobArray[1]=='12' && dobArray[2] <='21') {
-        pam.classList.add('hidden')
-        dwight.classList.add('hidden')
-        jim.classList.add('hidden')
-        michael.classList.remove('hidden')
-        angela.classList.add('hidden')
-        kevin.classList.add('hidden')
-        phyllis.classList.add('hidden')
-        meredith.classList.add('hidden')
-        stanley.classList.add('hidden')
-        ryan.classList.add('hidden')
-        kelly.classList.add('hidden')
-        toby.classList.add('hidden')
-    } 
 }
 document.querySelector('#generate').addEventListener('click', generate)
