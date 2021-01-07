b8bcf0f50cce3317bfaf6cca2ae1d0de52a0e7de
Justin Cho <justch0616@gmail.com>
2021-01-07 14:23:39 -0600

    Deleted unused JS and added missing Phyllis case

--- cho-cho/horoscopeApp/js/main.js
+++ cho-cho/horoscopeApp/js/main.js
@@ -1,60 +1,38 @@
-let peeps = []
-let dwight = document.getElementById('dwight')
-let jim = document.getElementById('jim')
-let michael = document.getElementById('michael')
-let angela = document.getElementById('angela')
-let kevin = document.getElementById('kevin')
-let meredith = document.getElementById('meredith')
-let phyllis = document.getElementById('phyllis')
-let stanley = document.getElementById('stanley')
-let ryan = document.getElementById('ryan')
-let kelly = document.getElementById('kelly')
-let toby = document.getElementById('toby')
-let pam = document.getElementById('pam')
-
-peeps.push(dwight, jim, michael, angela, kevin, meredith, phyllis, stanley, ryan, kelly, toby, pam)
-
+let peeps = document.getElementsByClassName('peeps')
 
 /*FUNCTION*/
 
 const generate = () => {
     let dob = document.querySelector('#dob').value;
     let dobArray = dob.split('-');
-    peeps.forEach(peep => peep.classList.add('hidden'));
+    for (let peep of peeps) {
+        peep.classList.add('hidden');
+    }
     switch(true) {
         case dobArray[1]=='01' && dobArray[2]<= '19' || dobArray[1]=='12' && dobArray[2] >='22':
-            dwight.classList.remove('hidden');
-            break;
+            return dwight.classList.remove('hidden');
         case dobArray[1]=='01' && dobArray[2]>= '20' || dobArray[1]=='02' && dobArray[2] <='18':
-            pam.classList.remove('hidden');
-            break;
+            return pam.classList.remove('hidden');
         case dobArray[1]=='02' && dobArray[2]>= '19' || dobArray[1]=='03' && dobArray[2] <='20':
-            kelly.classList.remove('hidden');
-            break;
+            return kelly.classList.remove('hidden');
         case dobArray[1]=='03' && dobArray[2]>= '21' || dobArray[1]=='04' && dobArray[2] <='19':
-            meredith.classList.remove('hidden');
-            break;
+            return meredith.classList.remove('hidden');
         case dobArray[1]=='04' && dobArray[2]>= '20' || dobArray[1]=='05' && dobArray[2] <='20':
-            kevin.classList.remove('hidden');
-            break;
+            return kevin.classList.remove('hidden');
         case dobArray[1]=='05' && dobArray[2]>= '21' || dobArray[1]=='06' && dobArray[2] <='20':
-            ryan.classList.remove('hidden');
-            break;
+            return ryan.classList.remove('hidden');
         case dobArray[1]=='06' && dobArray[2]>= '21' || dobArray[1]=='07' && dobArray[2] <='22':
-            stanley.classList.remove('hidden');
-            break;
+            return stanley.classList.remove('hidden');
         case dobArray[1]=='07' && dobArray[2]>= '23' || dobArray[1]=='08' && dobArray[2] <='22':
-            jim.classList.remove('hidden');
-            break;
+            return jim.classList.remove('hidden');
         case dobArray[1]=='08' && dobArray[2]>= '23' || dobArray[1]=='09' && dobArray[2] <='22':
-            toby.classList.remove('hidden');
-            break;
+            return phyllis.classList.remove('hidden');
+        case dobArray[1]=='09' && dobArray[2]>= '23' || dobArray[1]=='10' && dobArray[2] <='22':
+            return toby.classList.remove('hidden');
         case dobArray[1]=='10' && dobArray[2]>= '23' || dobArray[1]=='11' && dobArray[2] <='21':
-            angela.classList.remove('hidden');
-            break;
+            return angela.classList.remove('hidden');
         case dobArray[1]=='11' && dobArray[2]>= '22' || dobArray[1]=='12' && dobArray[2] <='21':
-            michael.classList.remove('hidden');
-            break;
+            return michael.classList.remove('hidden');
         default:
             console.log('I DECLARE..BANKRUPTCY!!!') 
     }
